a0648565162ccbdfe4305cf24b2a84cc2c0de601
Luca Landry <lucalandry@icloud.com>
2021-04-22 08:59:57 +0200

    frontend init

---
+++ src/front-end/CryptExFrontEnd/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
