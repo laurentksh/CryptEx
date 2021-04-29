fad5e5f2666ec99d133d209cfd758bf6fc32ba2f
Ethan <marchand.ethan@bluewin.ch>
2021-04-29 09:16:06 +0200

    Footer & Module Comp

--- src/front-end/CryptExFrontEnd/src/app/app.module.ts
+++ src/front-end/CryptExFrontEnd/src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CryptoModule } from './crypto/crypto.module';
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CryptoModule,
+    AuthModule,
+    MainModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/front-end/CryptExFrontEnd/src/app/main/main.module.ts
+++ src/front-end/CryptExFrontEnd/src/app/main/main.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './Components/home/home.component';
 import { ContactComponent } from './Components/contact/contact.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, ContactComponent],
+  declarations: [
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ]
 })
 export class MainModule { }
