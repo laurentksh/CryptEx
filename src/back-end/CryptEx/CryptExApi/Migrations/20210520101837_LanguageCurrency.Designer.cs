86354603e17133892a9fbacf5c3f2314da90d1f4
Laurent <25180858+laurentksh@users.noreply.github.com>
2021-05-20 12:19:04 +0200

    New Migration

--- src/back-end/CryptEx/CryptExApi/Migrations/20210520101837_LanguageCurrency.Designer.cs
+++ src/back-end/CryptEx/CryptExApi/Migrations/20210520101837_LanguageCurrency.Designer.cs
@@ -4,14 +4,16 @@ using CryptExApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CryptExApi.Migrations
 {
     [DbContext(typeof(CryptExDbContext))]
-    partial class CryptExDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210520101837_LanguageCurrency")]
+    partial class LanguageCurrency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
