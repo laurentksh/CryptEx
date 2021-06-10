387ae28cccfe584c5555678831d4a107be943d51
Luca Landry <lucalandry@icloud.com>
2021-06-10 13:30:55 +0200

    dto's

--- src/front-end/CryptExFrontEnd/src/app/user/Components/my-account/my-account.component.ts
+++ src/front-end/CryptExFrontEnd/src/app/user/Components/my-account/my-account.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AddressViewModel} from "../../models/address-view-model";
-import {UserViewModel} from "../../models/user-view-model";
 import {IbanViewModel} from "../../models/iban-view-model";
 import {Router} from "@angular/router";
 import {UserService} from "../../services/user.service";
+import {UserUpdateDto} from "../../models/user-update-dto";
+import {SnackbarService} from "../../../services/snackbar.service";
+import {AlertType, SnackBarCreate} from "../../../components/snackbar/snack-bar";
+import {UserViewModel} from "../../models/user-view-model";
+import {AddressDto} from "../../models/address-dto";
+import {IbanDto} from "../../models/iban-dto";
 
 @Component({
   selector: 'app-my-account',
@@ -11,33 +16,56 @@ import {UserService} from "../../services/user.service";
   styleUrls: ['./my-account.component.scss']
 })
 export class MyAccountComponent implements OnInit {
-  address: AddressViewModel;
-  user: UserViewModel;
-  userAdresse: AddressViewModel;
+  addressVm: AddressViewModel;
+  userUpdateDto: UserUpdateDto;
+  userVm : UserViewModel;
+  adresseDto: AddressDto;
   iban: IbanViewModel;
+  ibanDto: IbanDto;
 
-  clicked: boolean = false;
+  clickedUser: boolean = false;
   clickedPhone: boolean = false;
   clickedEmail: boolean = false;
   clickedAddress: boolean = false;
   clickedCountry: boolean = false;
   clickedIban: boolean = false;
 
-  constructor(private router: Router, private userService: UserService) {
-    this.address = {} as AddressViewModel;
-    this.user = {} as UserViewModel;
-    this.userAdresse = {} as AddressViewModel;
+  constructor(private router: Router, private userService: UserService, private snack: SnackbarService) {
+    this.addressVm = {} as AddressViewModel;
+    this.userUpdateDto = {} as UserUpdateDto;
     this.iban = {} as IbanViewModel;
+    this.userVm = {} as UserViewModel;
+    this.adresseDto = {}  as AddressDto;
+    this.ibanDto = {} as IbanDto;
   }
 
   ngOnInit(): void {
     this.userService.RefreshUser().then(x => {
-      this.user = this.userService.User;
+      this.userVm = this.userService.User;
     })
   }
 
-  updatePhoneNumber(): void {
+  updateUser(): void {
+    this.userService.UpdateUser(this.userUpdateDto).then(x =>
+    {
+      if (!x.success)
+      {
+        this.snack.ShowSnackbar(new SnackBarCreate("Error", "Update user error : " + x.error.message, AlertType.Error));
+      } else {
+        this.snack.ShowSnackbar(new SnackBarCreate("Error", "Update succeed", AlertType.Success));
+      }
+    })
+  }
 
+  doClick(): void{
+    this.clickedUser = !this.clickedUser;
+    this.clickedPhone = !this.clickedPhone;
+    this.clickedCountry = !this.clickedCountry;
+    this.clickedEmail = !this.clickedEmail;
+    this.clickedAddress = !this.clickedAddress;
+    this.clickedIban = !this.clickedIban;
+
+    this.updateUser();
   }
 
 
